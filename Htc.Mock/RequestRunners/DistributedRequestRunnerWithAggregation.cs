b799bd9d98eec6b4ac51c01f3a0d415c7f32c9a3
ddubuc <ddubuc@aneo.fr>
2021-10-29 09:22:10 +0200

    Move to best place all Customer interface

--- Htc.Mock/RequestRunners/DistributedRequestRunnerWithAggregation.cs
+++ Htc.Mock/RequestRunners/DistributedRequestRunnerWithAggregation.cs
@@ -1,21 +1,21 @@
 ﻿/* DistributedRequestRunnerWithAggregation.cs is part of the Htc.Mock solution.
-    
-   Copyright (c) 2021-2021 ANEO. 
+
+   Copyright (c) 2021-2021 ANEO.
      W. Kirschenmann (https://github.com/wkirschenmann)
-  
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-   
+
        http://www.apache.org/licenses/LICENSE-2.0
-   
+
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
 
-*/ 
+*/
 
 
 using System;
@@ -23,6 +23,7 @@ using System.Linq;
 using System.Text;
 
 using Htc.Mock.Core;
+using Armonik.sdk;
 
 using JetBrains.Annotations;
 
